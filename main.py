ad7e13bac9ea89a7efc5d10f687b0545fc8afc2b
Dylan Stitt <dstitt527@gmail.com>
2024-10-28 13:43:31 -0400

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+# Dylan Stitt
+# Unit 3 Lab 2
+# n Factorial
+
+def forFactorial(n):
+    ...
+
+def whileFactorial(n):
+    ...
+
+def recursiveFactorial(n):
+    ...
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
